b9405af60105fd9156934e411c465388531b5877
Steven <stevenpez44@gmail.com>
2021-06-22 15:45:20 -0400

    Initial Ursina set-up, has cube with rotations and some early buttons

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from visHints import *
 import random
 
 anim = False  # used to lockout inputs during animation
+blinking = False
 # drag = False  # used to rotate cube with mouse
 reading = False # used to lockout inputs during reading strings
 settings = False #checks if settings menu is open
@@ -210,6 +211,22 @@ def rotateD(): #rotates downward
         cube.rotateYY()
         invoke(endAnim, delay=.55)
 
+def rotateZL(): #rotates right
+    global anim
+    global reading
+    if not anim and not reading:
+        cube.rotateZ()
+        anim = True
+        invoke(endAnim, delay=.55)
+
+def rotateZR(): #rotates right
+    global anim
+    global reading
+    if not anim and not reading:
+        cube.rotateZZ()
+        anim = True
+        invoke(endAnim, delay=.55)
+
 
 def resetCube():  # resets cube
     global cube
@@ -237,6 +254,8 @@ def openSettings(): #open settings menu
         background.enabled = True
         inputButton.enabled = True
         scrambleButton.enabled = True
+        rotateZRButton.enabled = True
+        rotateZLButton.enabled = True
         settings = True
     else:
         resetButton.enabled = False
@@ -247,16 +266,23 @@ def openSettings(): #open settings menu
         background.enabled = False
         inputButton.enabled = False
         scrambleButton.enabled = False
+        rotateZRButton.enabled = False
+        rotateZLButton.enabled = False
         settings = False
 
 def openHints(): #open hints menu
     global hints
+    global blinking
     if not hints:
         hintDisplay.enabled = True
         hints = True
     else:
         hintDisplay.enabled = False
         hints = False
+
+    if blinking:
+        blinking = False
+        cube.unblink()
 
 
 
@@ -359,24 +385,43 @@ def scramble():  # creates a random string of moves
     scrambled_moves = "".join(random.choices(moves, k=25))
     readString(scrambled_moves, True)
 
+def hintMove():
+    global blinking
+    if blinking:
+        blinking = False
+        cube.unblink()
+    else:
+        blinking = True
+        cube.blink()
+
+def hintDetailToggle():
+    if hintDetail.enabled:
+        hintDetail.enabled = False
+    else:
+        hintDetail.enabled = True
+
 
 # =============================================================UI buttons======================================================================
 # settings==============
 settingsButton = Button(text='', icon='gear', color=color.gray, scale=.075, position=(-.81, .43, 0),
                         on_click=Func(openSettings))
 background = Button(text='', color=color.gray, highlight_color=color.gray, pressed_color=color.gray,
-                    position=(-.75, .12, 1), scale=(.25, .5), collider='', enabled=False)
-resetButton = Button(text='Reset', color=color.red, scale=.1, position=(-.81, .06, 0), on_click=Func(resetCube),
+                    position=(-.75, 0.06, 1), scale=(.25, .63), collider='', enabled=False)
+resetButton = Button(text='Reset', color=color.red, scale=.1, position=(-.81, -.18, 0), on_click=Func(resetCube),
                      enabled=False)
-rotateRButton = Button(text='', icon='rotateR', color=color.white, highlight_color=color.light_gray, scale=.1, position=(-.81, .18, 0),
+rotateRButton = Button(text='', icon='rotateR', color=color.white, highlight_color=color.light_gray, scale=.1, position=(-.69, .18, 0),
                        on_click=Func(rotateR), enabled=False)
-rotateLButton = Button(text='', icon='rotateL', color=color.white, highlight_color=color.light_gray, scale=.1, position=(-.69, .18, 0),
+rotateLButton = Button(text='', icon='rotateL', color=color.white, highlight_color=color.light_gray, scale=.1, position=(-.81, .18, 0),
                        on_click=Func(rotateL), enabled=False)
 rotateUButton = Button(text='', icon='rotateU', color=color.white, highlight_color=color.light_gray, scale=.1, position=(-.81, .3, 0),
                        on_click=Func(rotateU), enabled=False)
 rotateDButton = Button(text='', icon='rotateD', color=color.white, highlight_color=color.light_gray, scale=.1, position=(-.69, .3, 0),
                        on_click=Func(rotateD), enabled=False)
-inputButton = Button(text='Input', color=color.red, scale=.1, position=(-.69, .06, 0), on_click=Func(toggleInput),
+rotateZRButton = Button(text='', icon='rotateZR', color=color.white, highlight_color=color.light_gray, scale=.1, position=(-.69, .06, 0),
+                       on_click=Func(rotateZR), enabled=False)
+rotateZLButton = Button(text='', icon='rotateZL', color=color.white, highlight_color=color.light_gray, scale=.1, position=(-.81, .06, 0),
+                       on_click=Func(rotateZL), enabled=False)
+inputButton = Button(text='Input', color=color.red, scale=.1, position=(-.69, -.06, 0), on_click=Func(toggleInput),
                      enabled=False)
 inputList = TextField(max_lines=1, position=(-.75 ,-.3 ,0), enabled=False)
 scrambleButton = Button(text='Scramble', color=color.red, scale=.1, position=(-.81, -.06, 0), on_click=Func(scramble),
@@ -385,6 +430,13 @@ scrambleButton = Button(text='Scramble', color=color.red, scale=.1, position=(-.
 hintButton = Button(text='', icon='hintButton', color=color.gray, scale=.075, position=(-.81, -.43, 0), on_click=Func(openHints))
 hintDisplay = Button(text='', icon='hint1', color=color.gray, highlight_color=color.gray, pressed_color=color.gray,
                     position=(.6, .35, 1), scale=(.5, .25), collider='', enabled=False)
+hintMoveButton = Button(text='Show Move', icon='', color=color.dark_gray, scale=(.3,.15), position=(-.3, -.375, -1), parent=hintDisplay, on_click=Func(hintMove))
+hintDetailButton = Button(text='Details', icon='', color=color.dark_gray, scale=(.2,.15), position=(-.0, -.375, -1), parent=hintDisplay, on_click=Func(hintDetailToggle))
+hintDetail = Button(text='', icon='HintDetail1', color=color.gray, highlight_color=color.gray, pressed_color=color.gray,
+                    position=(-0, -1.85, 1), scale=(1.1, 2.64), collider='', parent = hintDisplay, enabled=False)
+
+
+
 
 if __name__ == '__main__':
     main()
--- visCube.py
+++ visCube.py
@@ -2,12 +2,40 @@
 from ursina import *
 from ursina.shaders import unlit_shader
 from ursina.shaders import lit_with_shadows_shader
+from ursina.curve import *
 
 class VisCube(Entity):
     cubes = [] #list of cublets
     arrows = [] #ui buttons
     center = Entity(x=0, y=0, z=0) #central entity, used for rotations
     anim = False
+    blinkSeq = Sequence()
+    e1 = Entity()
+    e2 = Entity()
+    e3 = Entity()
+    e4 = Entity()
+    e5 = Entity()
+    e6 = Entity()
+    e7 = Entity()
+    e8 = Entity()
+    e9 = Entity()
+    e10 = Entity()
+    e11 = Entity()
+    e12 = Entity()
+    e13 = Entity()
+    e14 = Entity()
+    e15 = Entity()
+    e16 = Entity()
+    e17 = Entity()
+    e18 = Entity()
+    e19 = Entity()
+    e20 = Entity()
+    e21 = Entity()
+    e22 = Entity()
+    e23 = Entity()
+    e24 = Entity()
+    e25 = Entity()
+    e26 = Entity()
 
     def __init__(self):
         super().__init__(
@@ -146,60 +174,60 @@ class VisCube(Entity):
 
         #=====================================creation of cublets=====================================
 
-        e1 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=0, z=-1, world_scale=(1, 1, 1), parent=self)
-        e2 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=0, z=-1, world_scale=(1, 1, 1), parent=self)
-        e3 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=0, z=-1, world_scale=(1, 1, 1), parent=self)
-        e4 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=1, z=-1, world_scale=(1, 1, 1), parent=self)
-        e5 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=-1, z=-1, world_scale=(1, 1, 1), parent=self)
-        e6 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=1, z=-1, world_scale=(1, 1, 1), parent=self)
-        e7 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=-1, z=-1, world_scale=(1, 1, 1), parent=self)
-        e8 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=1, z=-1, world_scale=(1, 1, 1), parent=self)
-        e9 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=-1, z=-1, world_scale=(1, 1, 1), parent=self)
+        self.e1 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=0, z=-1, world_scale=(1, 1, 1), parent=self)
+        self.e2 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=0, z=-1, world_scale=(1, 1, 1), parent=self)
+        self.e3 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=0, z=-1, world_scale=(1, 1, 1), parent=self)
+        self.e4 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=1, z=-1, world_scale=(1, 1, 1), parent=self)
+        self.e5 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=-1, z=-1, world_scale=(1, 1, 1), parent=self)
+        self.e6 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=1, z=-1, world_scale=(1, 1, 1), parent=self)
+        self.e7 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=-1, z=-1, world_scale=(1, 1, 1), parent=self)
+        self.e8 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=1, z=-1, world_scale=(1, 1, 1), parent=self)
+        self.e9 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=-1, z=-1, world_scale=(1, 1, 1), parent=self)
 
-        e10 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=1, z=0, world_scale=(1, 1, 1), parent=self)
-        e11 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=1, z=0, world_scale=(1, 1, 1), parent=self)
-        e12 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=1, z=0, world_scale=(1, 1, 1), parent=self)
-        e13 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=1, z=1, world_scale=(1, 1, 1), parent=self)
-        e14 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=1, z=1, world_scale=(1, 1, 1), parent=self)
-        e15 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=1, z=1, world_scale=(1, 1, 1), parent=self)
-        e16 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=0, z=0, world_scale=(1, 1, 1), parent=self)
-        e17 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=-1, z=0, world_scale=(1, 1, 1), parent=self)
-        e18 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=0, z=1, world_scale=(1, 1, 1), parent=self)
-        e19 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=-1, z=1, world_scale=(1, 1, 1), parent=self)
-        e20 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=0, z=0, world_scale=(1, 1, 1), parent=self)
-        e21 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=-1, z=0, world_scale=(1, 1, 1), parent=self)
-        e22 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=0, z=1, world_scale=(1, 1, 1), parent=self)
-        e23 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=-1, z=1, world_scale=(1, 1, 1), parent=self)
-        e24 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=-1, z=0, world_scale=(1, 1, 1), parent=self)
-        e25 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=-1, z=1, world_scale=(1, 1, 1), parent=self)
-        e26 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=-0, z=1, world_scale=(1, 1, 1), parent=self)
+        self.e10 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=1, z=0, world_scale=(1, 1, 1), parent=self)
+        self.e11 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=1, z=0, world_scale=(1, 1, 1), parent=self)
+        self.e12 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=1, z=0, world_scale=(1, 1, 1), parent=self)
+        self.e13 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=1, z=1, world_scale=(1, 1, 1), parent=self)
+        self.e14 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=1, z=1, world_scale=(1, 1, 1), parent=self)
+        self.e15 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=1, z=1, world_scale=(1, 1, 1), parent=self)
+        self.e16 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=0, z=0, world_scale=(1, 1, 1), parent=self)
+        self.e17 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=-1, z=0, world_scale=(1, 1, 1), parent=self)
+        self.e18 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=0, z=1, world_scale=(1, 1, 1), parent=self)
+        self.e19 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=1, y=-1, z=1, world_scale=(1, 1, 1), parent=self)
+        self.e20 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=0, z=0, world_scale=(1, 1, 1), parent=self)
+        self.e21 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=-1, z=0, world_scale=(1, 1, 1), parent=self)
+        self.e22 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=0, z=1, world_scale=(1, 1, 1), parent=self)
+        self.e23 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=-1, y=-1, z=1, world_scale=(1, 1, 1), parent=self)
+        self.e24 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=-1, z=0, world_scale=(1, 1, 1), parent=self)
+        self.e25 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=-1, z=1, world_scale=(1, 1, 1), parent=self)
+        self.e26 = Entity(model=load_model(name='cubetest'), color=color.rgb(200, 200, 200, 255), texture="RubiksTex", shader=lit_with_shadows_shader, x=0, y=-0, z=1, world_scale=(1, 1, 1), parent=self)
 
-        self.cubes.append(e1)
-        self.cubes.append(e2)
-        self.cubes.append(e3)
-        self.cubes.append(e4)
-        self.cubes.append(e5)
-        self.cubes.append(e6)
-        self.cubes.append(e7)
-        self.cubes.append(e8)
-        self.cubes.append(e9)
-        self.cubes.append(e10)
-        self.cubes.append(e11)
-        self.cubes.append(e12)
-        self.cubes.append(e13)
-        self.cubes.append(e14)
-        self.cubes.append(e15)
-        self.cubes.append(e16)
-        self.cubes.append(e17)
-        self.cubes.append(e18)
-        self.cubes.append(e19)
-        self.cubes.append(e20)
-        self.cubes.append(e21)
-        self.cubes.append(e22)
-        self.cubes.append(e23)
-        self.cubes.append(e24)
-        self.cubes.append(e25)
-        self.cubes.append(e26)
+        self.cubes.append(self.e1)
+        self.cubes.append(self.e2)
+        self.cubes.append(self.e3)
+        self.cubes.append(self.e4)
+        self.cubes.append(self.e5)
+        self.cubes.append(self.e6)
+        self.cubes.append(self.e7)
+        self.cubes.append(self.e8)
+        self.cubes.append(self.e9)
+        self.cubes.append(self.e10)
+        self.cubes.append(self.e11)
+        self.cubes.append(self.e12)
+        self.cubes.append(self.e13)
+        self.cubes.append(self.e14)
+        self.cubes.append(self.e15)
+        self.cubes.append(self.e16)
+        self.cubes.append(self.e17)
+        self.cubes.append(self.e18)
+        self.cubes.append(self.e19)
+        self.cubes.append(self.e20)
+        self.cubes.append(self.e21)
+        self.cubes.append(self.e22)
+        self.cubes.append(self.e23)
+        self.cubes.append(self.e24)
+        self.cubes.append(self.e25)
+        self.cubes.append(self.e26)
 
     def reparentCube(self): #reparent cublets from the center to the main cube
         for e in self.cubes:
@@ -375,6 +403,34 @@ class VisCube(Entity):
             e.reparent_to(self.center)
         self.center.animate('rotation_x', self.center.rotation_x - 90, duration=.5)
         invoke(self.reparentCube, delay=.55)
+
+    def rotateZ(self):
+        for e in self.cubes:
+            e.reparent_to(self.center)
+        self.center.animate('rotation_z', self.center.rotation_z - 90, duration=.5)
+        invoke(self.reparentCube, delay=.55)
+
+    def rotateZZ(self):
+        for e in self.cubes:
+            e.reparent_to(self.center)
+        self.center.animate('rotation_z', self.center.rotation_z + 90, duration=.5)
+        invoke(self.reparentCube, delay=.55)
+
+    def blink(self):
+        self.blinkSeq.append(Func(self.e1.blink, value=color.black,duration=2))
+        self.blinkSeq.append(Func(self.e2.blink, value=color.black, duration=2))
+        self.blinkSeq.append(Func(self.e3.blink, value=color.black, duration=2))
+        self.blinkSeq.append(Func(self.e4.blink, value=color.black, duration=2))
+        self.blinkSeq.append(Func(self.e5.blink, value=color.black, duration=2))
+
+        self.blinkSeq.loop = True
+        self.blinkSeq.append(2)
+        self.blinkSeq.start()
+
+    def unblink(self):
+        self.blinkSeq.kill()
+        self.blinkSeq.loop = False
+        self.blinkSeq = Sequence()
 
     def arrowFunc(self,func): #activates when an arrow is clicked, does the passed function, disables arrows during animation
         self.anim = True
