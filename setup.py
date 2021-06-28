2e58fd4aa803bb06281a03287638b10c4646704f
Andrew Penton <apenton@ufl.edu>
2021-06-28 11:48:19 -0500

    moved files for pip distribution

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 setup(
 	name="QB-solver",
-	version="0.0.1",
-	packages=["",],	# name of modules
+	version="0.1.2",
+	packages=["src",],	# name of modules
 	url="https://github.com/a-penton/QB",
 	# license
 	author="Andrew Penton, Heinrich Perez, Steven Perez, Noah Sharpe, Daniel Shinkarow",
 	# author_email
 	description="Rubik's cube simulation and tutorial",
-	install_requires=["ursina"],
+	install_requires=["ursina", "rubik-cube"],
 
 	entry_points = 
 	{ "console_scripts":
 		[
-			"run_simulation = src:main",
+			"run_simulation = src.main:main",
 			#"command_name_2 = src.file_name:func_name"
 		]
 	}
