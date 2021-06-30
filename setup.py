ff4222cce5cd9e3ab3f9fe4ebb05dc53d1b2d25a
Andrew Penton <apenton@ufl.edu>
2021-06-30 12:26:21 -0500

    got resources to show up

--- setup.py
+++ setup.py
@@ -2,8 +2,17 @@ from setuptools import setup
 
 setup(
 	name="QB-solver",
-	version="0.1.4.6.5",
+	version="0.1.4.7.8",
 	packages=["qb_solver",],	# name of modules
+	package_data={
+		"qb_solver": [
+			"Models/*.obj",
+			"Models/*.png",
+			"Models/Notation/*.png",
+			"hints/*.png"
+		],
+	},
+	include_package_data=True,	# make sure resources are included
 	url="https://github.com/a-penton/QB",
 	# license
 	author="Andrew Penton, Heinrich Perez, Steven Perez, Noah Sharpe, Daniel Shinkarow",
