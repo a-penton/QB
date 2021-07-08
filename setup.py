d537ffd88d22994d18e04945c12bcc9cc5769518
Andrew Penton <andrew.penton@gmail.com>
2021-07-08 16:40:31 -0500

    added license and author email, tried to get run_simulation working

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="QB-solver",
-	version="0.1.4.8.5",
+	version="0.1.4.8.8",
 	packages=["qb_solver",],	# name of modules
 	package_data={
 		"qb_solver": [
@@ -14,17 +14,16 @@ setup(
 	},
 	include_package_data=True,	# make sure resources are included
 	url="https://github.com/a-penton/QB",
-	# license
+	license = "GPL3",
 	author="Andrew Penton, Heinrich Perez, Steven Perez, Noah Sharpe, Daniel Shinkarow",
-	# author_email
+	author_email = "andrew.penton@gmail.com",
 	description="Rubik's cube simulation and tutorial",
 	install_requires=["ursina", "rubik-cube", "psd-tools3"],
 
 	entry_points = 
 	{ "console_scripts":
 		[
-			"run_simulation=qb_solver.__main__:main",
-			#"command_name_2 = qb_solver.file_name:func_name"
+			"run_simulation = qb_solver.__main__:main"
 		]
 	}
 )
